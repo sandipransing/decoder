2065217480fdd433a88e4ff43c49ca9775d329e4
Brian Cardarella <bcardarella@gmail.com>
2009-08-31 03:52:32 -0400

    Added Mocha to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
+require 'ruby-debug'
+require 'mocha'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
