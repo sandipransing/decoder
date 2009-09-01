0161b9aaf3098db6e82c5664be615ff4863a2e46
Brian Cardarella <bcardarella@gmail.com>
2009-09-01 12:15:38 -0400

    Renamed EmptyTest to EmptyClass

--- test/common_methods_test.rb
+++ test/common_methods_test.rb
@@ -2,23 +2,23 @@ require 'test_helper'
 
 class CommonMethodsTest < Test::Unit::TestCase
 
-  class EmptyTest
+  class EmptyClass
     include CommonMethods
     attr_accessor :code, :name
   end
   
   context "CommonMethods" do
     setup do
-      @empty_test = EmptyTest.new
-      @empty_test.code = "TS"
-      @empty_test.name = "TEST"
+      @empty_class = EmptyClass.new
+      @empty_class.code = "TS"
+      @empty_class.name = "TEST"
     end
     should "print #name for #to_s" do
-      assert_equal @empty_test.name, @empty_test.to_s
+      assert_equal @empty_class.name, @empty_class.to_s
     end
   
     should "print '#<#class code: code, #name: #name>" do
-      assert_equal "#<CommonMethodsTest::EmptyTest code: TS, name: TEST>", @empty_test.inspect
+      assert_equal "#<CommonMethodsTest::EmptyClass code: TS, name: TEST>", @empty_class.inspect
     end
   end
 end
